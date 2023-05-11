88a25cfce5c61c6d9e70162d3a0ab7d77683a90e
Victor Hugo da Soler <dasolervictor@hotmail.com>
2023-05-10 21:38:57 -0300

    feature/002 - Criada função de subtração

--- calc-python.py
+++ calc-python.py
@@ -1,9 +1,13 @@
 print("Calculadora Python")
 try:
+    operation = input("Digite a operação que deseja realizar ('+' ou '-'): ")
     number1 = int(input("Digite o primeiro número: "))
     number2 = int(input("Digite o segundo número: "))
-
-    print(f"{number1}+{number2}={number1 + number2}")
-
+    if operation == "+":
+        print(f"{number1}+{number2}={number1 + number2}")
+    elif operation == "-":
+        print(f"{number1}+{number2}={number1 + number2}")
+    else:
+        print("Operação Inválida, digite um operador válido.")
 except:
     print("Número inválido")
