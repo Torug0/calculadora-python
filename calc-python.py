b056f8ce8ab9e4be815a6c9e2dd8a1a1fbad6ddb
Victor Hugo da Soler <dasolervictor@hotmail.com>
2023-05-10 21:46:54 -0300

    feature/003 - Criada função de multiplicação

--- calc-python.py
+++ calc-python.py
@@ -1,13 +1,19 @@
 print("Calculadora Python")
-try:
-    operation = input("Digite a operação que deseja realizar ('+' ou '-'): ")
-    number1 = int(input("Digite o primeiro número: "))
-    number2 = int(input("Digite o segundo número: "))
-    if operation == "+":
-        print(f"{number1}+{number2}={number1 + number2}")
-    elif operation == "-":
-        print(f"{number1}+{number2}={number1 + number2}")
-    else:
-        print("Operação Inválida, digite um operador válido.")
-except:
-    print("Número inválido")
+
+operation = input("Digite a operação que deseja realizar ('+', '-' ou '*'): ")
+
+if(operation != "+" or operation != "-" or operation != "*"):
+    print("Operação Inválida, digite um operador válido.")
+else: 
+    try:
+        number1 = int(input("Digite o primeiro número: "))
+        number2 = int(input("Digite o segundo número: "))
+
+        if operation == "+":
+            print(f"{number1}+{number2}={number1 + number2}")
+        elif operation == "-":
+            print(f"{number1}-{number2}={number1 - number2}")
+        elif operation == "*":
+            print(f"{number1}*{number2}={number1 * number2}")
+    except:
+        print("Número inválido")
